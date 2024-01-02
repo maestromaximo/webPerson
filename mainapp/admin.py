593974f992cbd853c17908e95ce41ac28509c001
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-01-02 16:36:12 -0500

    added to the admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,7 +3,7 @@ from .models import FieldEntry
 
 @admin.register(FieldEntry)
 class FieldEntryAdmin(admin.ModelAdmin):
-    list_display = ('date', 'time', 'money', 'read_status')
-    list_filter = ('read_status', 'date')
+    list_display = ('date', 'time', 'money', 'read_status', 'type')
+    list_filter = ('read_status', 'date', 'type')
     search_fields = ('message',)
     ordering = ('-date', '-time')
