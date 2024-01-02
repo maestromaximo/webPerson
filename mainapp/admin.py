c97327bbb3ee749973deee46ed0664b0749a65b8
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-01-02 01:09:48 -0500

    admin for field entry

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FieldEntry
 
-# Register your models here.
+@admin.register(FieldEntry)
+class FieldEntryAdmin(admin.ModelAdmin):
+    list_display = ('date', 'time', 'money', 'read_status')
+    list_filter = ('read_status', 'date')
+    search_fields = ('message',)
+    ordering = ('-date', '-time')
