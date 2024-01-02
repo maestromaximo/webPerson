5ce054ed070cb4833e6ecba37da741fb316dac58
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-01-02 16:35:30 -0500

    added type to the model

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,8 +3,18 @@ from django.db import models
 # Create your models here.
 
 class FieldEntry(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ('deposit', 'Deposit'),
+        ('withdrawal', 'Withdrawal'),
+    ]
+
     date = models.DateField()
     time = models.TimeField()
     message = models.TextField()
     money = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(
+        max_length=10,
+        choices=TRANSACTION_TYPE_CHOICES,
+        default='withdrawal',
+    )
     read_status = models.BooleanField(default=False)
