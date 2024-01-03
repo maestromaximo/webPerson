fe20032fca0c3e915d8a1e51069fb576919de4a3
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-01-03 16:50:35 -0500

    camera

--- healthTracker/urls.py
+++ healthTracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home_food'),
     path('scanning/', views.barcode_scanning, name="barcode_scanning"),
+    path('api/barcode_info/', views.barcode_info, name='barcode_info')
 ]
--- healthTracker/views.py
+++ healthTracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
+import json, requests
 from datetime import datetime, timedelta
 
 def home(request):
@@ -11,3 +11,32 @@ def home(request):
 
 def barcode_scanning(request):
     return render(request, 'scanning_page.html')
+
+@csrf_exempt
+def barcode_info(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        barcode = data.get('barcode')
+
+        # Query the Open Food Facts API
+        api_url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            product_data = response.json().get('product', {})
+            product_name = product_data.get('product_name', 'Product name not available')
+            nutritional_info = product_data.get('nutriments', {})
+
+            response_data = {
+                'barcode': barcode,
+                'product_name': product_name,
+                'nutritional_info': nutritional_info
+            }
+        else:
+            response_data = {
+                'barcode': barcode,
+                'error': 'Product not found or API error'
+            }
+
+        return JsonResponse(response_data)
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
