65190a5b24294a7216034edfa0e429e9bedcf981
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-01-21 22:47:56 -0500

    Add chat view to urls.py

--- gptInterface/urls.py
+++ gptInterface/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.interface_menu, name='interface_menu'),
-
-
+    # path('chat/', views.chat, name='chat'),
+    path('chat/', views.chat_view, name='chat_view'),
+    path('chat/<slug:model>', views.chat, name='chat'),
 ]
