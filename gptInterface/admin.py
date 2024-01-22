99a538166b88851cd19ebed146e1254bcb7d1f98
maestromaximo <alejandrogarcia2423@hotmail.com>
2024-01-21 22:47:40 -0500

    Add ChatModel to admin interface

--- gptInterface/admin.py
+++ gptInterface/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ChatModel
 
-# Register your models here.
+@admin.register(ChatModel)
+class ChatModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'model_name')
+    prepopulated_fields = {'slug': ('name',)}
--- gptInterface/models.py
+++ gptInterface/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class ChatModel(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    notes = models.TextField(blank=True)
+    model_name = models.CharField(max_length=50, default='gpt-3.5-turbo-0613')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # If the slug is not set, generate it from the name
+        if not self.slug:
+            self.slug = slugify(self.name)
+            # Ensure the slug is unique and append a number if it's not
+            original_slug = self.slug
+            counter = 1
+            while ChatModel.objects.filter(slug=self.slug).exists():
+                self.slug = f'{original_slug}-{counter}'
+                counter += 1
+        super().save(*args, **kwargs)
